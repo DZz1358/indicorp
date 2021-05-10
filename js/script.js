0902406e45eef7645ac0a62ec0d6352de589943d
DenZ <https://github.com/DZz1358/Jkaros.git>
2021-05-10 21:35:16 +0300

    add nav + up burger

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
         $('.header__burger,.nav_list').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    
+    $('.nav_list a').on('click' , function(){
+        $('.header__burger,.nav_list').removeClass("active");
+    })
+
+
     let lastY;
     $(document).on('scroll', function (e) {
         let currentY = $(window).scrollTop();
-
         let header = $('.header');
         if (currentY > lastY) {
             header.addClass('header_bg');
@@ -15,11 +20,13 @@ $(document).ready(function() {
         }
         lastY = currentY;
     })
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
 
-    // let scrollHeight = Math.max(
-    //     document.body.scrollHeight, document.documentElement.scrollHeight,
-    //     document.body.offsetHeight, document.documentElement.offsetHeight,
-    //     document.body.clientHeight, document.documentElement.clientHeight
-    // ); 
+
 }); 
 
